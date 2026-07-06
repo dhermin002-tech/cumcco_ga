8e75ba9137141535a74bd72250a829c671421207
lucasokinda <nathlucas@outlook.fr>
2026-07-06 17:02:57 +0200

    correction de rdvcontroller

--- app/Http/Controllers/RendezVousController.php
+++ app/Http/Controllers/RendezVousController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\RendezVous;   // ← important : importer le modèle
+use App\Models\RendezVous;   
 
 class RendezVousController extends Controller
 {
